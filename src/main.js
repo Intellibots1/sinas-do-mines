c01805c07b649f19fa16c7c3fcc1adbb72071abf
Oyewolet <webcreationdev@gmail.com>
2023-07-04 15:05:37 -0300

    Initial interval bot

---
+++ src/main.js
@@ -0,0 +1,25 @@
+const { Telegraf } = require("telegraf");
+const express = require("express");
+const CronJob = require("cron").CronJob;
+require("dotenv").config();
+
+const app = express();
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+async function sendMessage(channel_id) {
+  await bot.telegram.sendSticker(
+    channel_id,
+    "CAACAgIAAxkBAAEJlG9kpAAB9P8OaBiUEtQdyZkUtmIh6uUAAgMBAAJWnb0KAuXReIfl-k8vBA"
+  );
+}
+
+const job = new CronJob("*/4 * * * *", () => {
+  sendMessage("-1001910500046");
+});
+
+bot.launch();
+job.start();
+
+app.listen(8080 || process.env.PORT, () => {
+  console.log(`Bot is running on http://localhost:8080`);
+});
