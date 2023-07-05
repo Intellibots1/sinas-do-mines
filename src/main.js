13e549db6b8fe6329c161b842b2b4fb71cf9bc4a
Oyewolet <webcreationdev@gmail.com>
2023-07-05 13:49:19 -0300

    Added one more sticker

--- src/main.js
+++ src/main.js
@@ -6,19 +6,31 @@ require("dotenv").config();
 const app = express();
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-async function sendMessage(channel_id) {
+async function sendSticker1(channel_id) {
   await bot.telegram.sendSticker(
     channel_id,
     "CAACAgIAAxkBAAEJlG9kpAAB9P8OaBiUEtQdyZkUtmIh6uUAAgMBAAJWnb0KAuXReIfl-k8vBA"
   );
 }
 
-const job = new CronJob("*/8 * * * *", () => {
-  sendMessage("-1001910500046");
+async function sendSticker2(channel_id) {
+  await bot.telegram.sendSticker(
+    channel_id,
+    "CAACAgIAAxkBAAEJmpFkpZzqQ1-hj24dc04_u5rez-Dc8wACNhYAAlxA2EvbRm7S3ZV6DS8E"
+  );
+}
+
+const job1 = new CronJob("*/8 * * * *", () => {
+  sendSticker1("-1001910500046");
+});
+
+const job2 = new CronJob("*/5 * * * *", () => {
+  sendSticker2("-1001910500046");
 });
 
 bot.launch();
-job.start();
+job1.start();
+job2.start();
 
 app.get("/play", (req, res) => {
   res.send({ ping: "pong ⚾" });
